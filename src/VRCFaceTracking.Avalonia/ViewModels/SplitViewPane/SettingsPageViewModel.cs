377eeb97bd61d39af0a5935cf67a893021913b21
dfgHiatus <51272212+dfgHiatus@users.noreply.github.com>
2025-02-09 15:37:59 -0600

    (feat) Add Hardware Debug

--- src/VRCFaceTracking.Avalonia/ViewModels/SplitViewPane/SettingsPageViewModel.cs
+++ src/VRCFaceTracking.Avalonia/ViewModels/SplitViewPane/SettingsPageViewModel.cs
@@ -1,18 +1,25 @@
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
+using Avalonia;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+using Avalonia.Threading;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.DependencyInjection;
-using Jeek.Avalonia.Localization;
-using VRCFaceTracking.Avalonia.Views;
 using VRCFaceTracking.Core.Contracts;
 using VRCFaceTracking.Core.Contracts.Services;
 using VRCFaceTracking.Core.Services;
 using VRCFaceTracking.Models;
 using VRCFaceTracking.Services;
+using Vector = Avalonia.Vector;
+using VrcftImage = VRCFaceTracking.Core.Types.Image;
 
 namespace VRCFaceTracking.Avalonia.ViewModels.SplitViewPane;
 
-public partial class SettingsPageViewModel : ViewModelBase
+public partial class SettingsPageViewModel : ViewModelBase, IDisposable
 {
     [ObservableProperty]
     [property: SavedSetting("CalibrationEnabled", false)]
@@ -48,6 +55,11 @@ public partial class SettingsPageViewModel : ViewModelBase
     public GithubService GithubService { get; private set;}
     public ParameterSenderService ParameterSenderService { get; private set;}
 
+    public bool HasUpperImage => UpperImageSource is not null;
+    public bool HasLowerImage => LowerImageSource is not null;
+    public WriteableBitmap UpperImageSource { get; private set; }
+    public WriteableBitmap LowerImageSource { get; private set; }
+
     public SettingsPageViewModel()
     {
         // General/Calibration Settings
@@ -60,14 +72,94 @@ public partial class SettingsPageViewModel : ViewModelBase
         // Risky Settings
         ParameterSenderService = Ioc.Default.GetService<ParameterSenderService>()!;
 
+        // Initialize hardware debug streams for upper and lower face tracking
+        UpperImageSource = InitializeHardwareDebugStream(UnifiedTracking.EyeImageData);
+        LowerImageSource = InitializeHardwareDebugStream(UnifiedTracking.LipImageData);
+
+        UnifiedTracking.OnUnifiedDataUpdated += _ => Dispatcher.UIThread.Invoke(OnTrackingDataUpdated);
+
         PropertyChanged += (_, _) =>
         {
             SettingsService.Save(this);
         };
     }
 
+    ~SettingsPageViewModel()
+    {
+        UnifiedTracking.OnUnifiedDataUpdated -= _ => Dispatcher.UIThread.Invoke(OnTrackingDataUpdated);
+    }
+
+    private WriteableBitmap InitializeHardwareDebugStream(VrcftImage image)
+    {
+        var imageSize = image.ImageSize;
+
+        if ( imageSize is { x: > 0, y: > 0 } )
+        {
+            return new WriteableBitmap(
+                new PixelSize(imageSize.x, imageSize.y),
+                new Vector(96, 96),
+                PixelFormat.Rgba8888,
+                AlphaFormat.Opaque);
+        }
+
+        return null;
+    }
+
+    private async void OnTrackingDataUpdated()
+    {
+        // Handle eye tracking
+        var upperData = UnifiedTracking.EyeImageData.ImageData;
+        if ( upperData != null )
+        {
+            UpperImageSource ??= InitializeHardwareDebugStream(UnifiedTracking.EyeImageData);
+
+            using var frameBuffer = UpperImageSource.Lock();
+            {
+                Marshal.Copy(upperData, 0, frameBuffer.Address, upperData.Length);
+            }
+        }
+        else
+        {
+            // Handle device getting unplugged / destroyed / disabled
+            // Device is connected
+            if ( UpperImageSource != null )
+            {
+                UpperImageSource.Dispose();
+                UpperImageSource = null;
+            }
+        }
+
+        // Handle lip tracking
+        var lowerData = UnifiedTracking.LipImageData.ImageData;
+        if ( lowerData != null )
+        {
+            LowerImageSource ??= InitializeHardwareDebugStream(UnifiedTracking.LipImageData);
+
+            using var frameBuffer = LowerImageSource.Lock();
+            {
+                Marshal.Copy(lowerData, 0, frameBuffer.Address, lowerData.Length);
+            }
+        }
+        else
+        {
+            // Handle device getting unplugged / destroyed / disabled
+            // Device is connected
+            if ( LowerImageSource != null )
+            {
+                LowerImageSource.Dispose();
+                LowerImageSource = null;
+            }
+        }
+    }
+
     private async Task LoadContributors()
     {
         Contributors = await GithubService.GetContributors("benaclejames/VRCFaceTracking");
+    }
+
+    public void Dispose()
+    {
+        UpperImageSource.Dispose();
+        LowerImageSource.Dispose();
     }
 }
